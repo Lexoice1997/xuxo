b8f3400a7263b0580ef3ea791cee2d1532674d1b
Azamat <lexoice1997@gmail.com>
2023-02-25 23:29:06 +0500

    fix bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Routs from './routes/Routs';
-import { setupStore } from './store/store';
-
-const store = setupStore();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Provider store={store}>
-        <div className="App">
-          <Routs />
-        </div>
-      </Provider>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
