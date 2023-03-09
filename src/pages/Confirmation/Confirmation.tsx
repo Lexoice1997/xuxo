53928184021ddc62a199c68a1b6d32a499ca93f3
Azamat <lexoice1997@gmail.com>
2023-03-10 00:31:03 +0500

    fix: fix bugs

--- src/pages/Confirmation/Confirmation.tsx
+++ src/pages/Confirmation/Confirmation.tsx
@@ -1,16 +1,12 @@
 import { ClockCircleOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from '../../helpers/hooks/redux';
-import { authSlice } from '../../store/slices/authSlice';
+import { useAppSelector } from '../../helpers/hooks/redux';
 import styles from './Confirmation.module.scss';
 
 function Confirmation() {
-  const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const { user } = useAppSelector((state) => state.authReducer);
-  const { handleActivate } = authSlice.actions;
 
   useEffect(() => {
     if (user?.isActive === 1) {
@@ -23,7 +19,6 @@ function Confirmation() {
       <ClockCircleOutlined style={{ fontSize: '70px', color: '#1890FF' }} />
       <h2>Потверждение аккаунта</h2>
       <p>Пожалуйста обратитесь к менеджеру для одобрения регистрации вашего аккаунта</p>
-      <Button onClick={() => dispatch(handleActivate())}>Activate</Button>
     </div>
   );
 }
